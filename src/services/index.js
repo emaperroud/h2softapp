0a096819448080dfdfd12b2c60d7b7c898da8a26
Vecchiet Nico <hnvecchiet@gmail.com>
2017-07-12 22:48:47 -0300

    Definicion de la entidad cliente y metodos CRUD

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const usuario = require('./usuario/usuario.service.js');
+const cliente = require('./cliente/cliente.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(usuario);
+  app.configure(cliente);
 };
